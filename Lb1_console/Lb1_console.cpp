25d59a2ed4fb94cdafbd8d075ca99a65d90b136e
Kirikosha <kirikoshamain@gmail.com>
2024-04-14 15:23:39 +0200

    almost finished lab

--- Lb1_console/Lb1_console.cpp
+++ Lb1_console/Lb1_console.cpp
@@ -1,34 +1,39 @@
-﻿
-#include <tchar.h>
+﻿#include <tchar.h>
 #include <iostream>
 #include <Windows.h>
+
 #ifdef UNICODE
 typedef		wchar_t	TCHAR;
 #define	TEXT(a)	L##a
 #define	_tcslen		wcslen
 #define _tcin wcin
+#define _tsetlocale _wsetlocale
 #else
 typedef 	char	   	TCHAR;
 #define 	TEXT(a)	a
 #define	_tcslen		strlen
 #define _tcin cin
+#define _tsetlocale setlocale
 #endif
-
+using namespace std;
 bool checkIfUnicode(TCHAR* text);
 size_t getLengthInBytes(TCHAR* text);
+void parents();
 
 int main()
 {
-    using namespace std;
+    SetConsoleOutputCP(CP_UTF8);
+    _tsetlocale(LC_ALL, _T("Ukrainian"));
     TCHAR* text = new TCHAR[256]; // Allocate memory for input
     _tcin.getline(text, 256);
     if (checkIfUnicode(text)) {
-        cout << "It is unicode";
-    }
-    else {
         cout << "It is ASCII";
     }
+    else {
+        cout << "It is unicode";
+    }
 
+    parents();
     return 0;
 }
 
@@ -45,4 +50,12 @@ bool checkIfUnicode(TCHAR* text)
         isUnicode = true;
     }
     return isUnicode;
+}
+
+void parents() {
+    const char* parents[] = {"Білецька", "Білецький"};
+    const int length = 2;
+    for (int i = 0; i < length; i++) {
+        cout << parents[i];
+    }
 }
